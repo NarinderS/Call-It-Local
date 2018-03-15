28de01e3d918d00bfd07e7e8b0c965ae5723a197
Brian Pho <brianpho7@gmail.com>
2018-03-15 03:05:07 -0600

    Changed UI and added more error checking

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -9,27 +9,27 @@ function checkInputs() {
 
     // Checks to make sure that none of the fields are empty 
     if (username.length == 0 || password.length == 0 || address.length == 0 || phonenumber.length == 0 || email.length == 0) {
-        alert("Please make sure all fields are filled");
+        alert("Please make sure all fields are filled.");
         return false;
     }
 
     // Ensures that the password has at least one capital letter and at least one number
     if (!(/\d/.test(password)) || !(/[A-Z]/.test(password))) {
-        alert("Please make sure your password contains at least one uppercase letter and one number");
+        alert("Please make sure your password contains at least one uppercase letter and one number.");
         return false;
     }
 
     // Checks to make sure the phone number is formatted properly
     var phoneRegex = /^[0-9]{10}$/;
     if (!phoneRegex.test(phonenumber)) {
-        alert("Please enter your number in the format 'XXXXXXXXXX'");
+        alert("Please enter your number in the format 'XXXXXXXXXX'.");
         return false;
     }
 
     // Checks to make sure that the email follows the proper format
     var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailRegex.test(email)) {
-        alert("Please enter a valid email address");
+        alert("Please enter a valid email address.");
         return false;
     }
 
@@ -38,13 +38,17 @@ function checkInputs() {
 }
 
 function checkUsernamePassword() {
-    var username = getElementById("username").value;
-    var password = getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-    if (username.length == 0 || password.length == 0) {
-        alert("Please do not leave the 'username'/ 'password' input(s) empty");
+    if (username.length == 0 ) {
+        alert("Please do not leave the username field empty.");
         return false;
     }
 
+    if (password.length == 0) {
+        alert("Please do not leave the password field empty.");
+        return false;
+    }
     return true;
 }
