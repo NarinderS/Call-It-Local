db353867fe7af1909e4ffee3c72dbc51042ea794
Unknown <harjeej5@gmail.com>
2018-03-14 22:29:02 -0600

    sdfdsfsdfds

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -2,19 +2,7 @@
 
 function checkInputs() {
 
-    var accountInfo = $('form').serialize();
+    var username = document.getElementById("username").value;
 
-    var splitString = accountInfo.split("&");
-
-    if (splitString.length !== 6) {
-        alert("Please make sure all fields are filled");
-    }
-
-    var userName = splitString[1];
-    var pass = splitString[2];
-    var address = splitString[3];
-    var phoneNumber = splitString[4];
-    var email = splitString[5];
-
-    alert(email);
+    alert(username);
 }
