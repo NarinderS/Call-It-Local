1a6c1d6ec760a8cc2cda020dd018a961123e88d8
Unknown <harjeej5@gmail.com>
2018-03-14 22:09:02 -0600

    Part 1 of checking done (hopefully)

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -1,7 +1,20 @@
 ﻿// To be completed by students in milestone 2
 
-function checkInputs() { 
+function checkInputs() {
 
-    var accountInfo = $('form').serializeArray();
-    alert(accountInfo);
+    var accountInfo = $('form').serialize();
+
+    var splitString = accountInfo.split("&");
+
+    if (splitString.length !== 6) {
+        alert("Please make sure all fields are filled");
+    }
+
+    var userName = splitString[1];
+    var pass = splitString[2];
+    var address = splitString[3];
+    var phoneNumber = splitString[4];
+    var email = splitString[5];
+
+    alert(email);
 }
