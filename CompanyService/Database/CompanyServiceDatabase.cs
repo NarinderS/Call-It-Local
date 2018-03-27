6fd4c65d48173e9fa055269f88eb7baba92a13de
Sadat Islam <sadat21@hotmail.ca>
2018-03-27 13:28:47 -0600

    Added Folders For CompanyService

---
+++ CompanyService/Database/CompanyServiceDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Messages;
+using Messages.Database;
+using Messages.DataTypes;
+using Messages.NServiceBus.Events;
+using Messages.ServiceBusRequest.Echo.Requests;
+
+using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoService.Database
+{
+    /// <summary>
+    /// This portion of the class contains methods and functions
+    /// </summary>
+    /*public partial class CompanyServiceDatabase : AbstractDatabase
+    {
+        
+    }*/
+
+    
+   
+}
