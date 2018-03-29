3888e2e5d9a1dba1adf6f0f62e9605ee36eff310
Unknown <harjeej5@gmail.com>
2018-03-28 21:43:38 -0600

    Added some stuff for the post request

--- ClientApplicationMVC/Controllers/CompanyReviewController.cs
+++ ClientApplicationMVC/Controllers/CompanyReviewController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Text;
 
 namespace ClientApplicationMVC.Controllers
 {
@@ -12,6 +14,15 @@ namespace ClientApplicationMVC.Controllers
         [HttpPost]
         public ActionResult PostCompanyReview(String companyName, String review, String stars, String username)
         {
+            HttpClient client = new HttpClient();
+
+            String json = "{\"companyname\":\"" + companyName + "\",\"review\":\"" + review + "\",\"stars\":\"" + stars + "\",timestamp\":\"" + DateTimeOffset.Now + "\",username\":\"" + username + "\"}";
+            
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // TODO: Put the actual URI for the assignment 4 instance here
+            var result = client.PostAsync("http://localhost/api/Reviews", content).Result;
+
             return View();
         }
     }
