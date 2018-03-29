5f38606789464a3aba0ada749ee7053a0d61476c
Unknown <harjeej5@gmail.com>
2018-03-28 22:25:03 -0600

    Get Request Added

--- ClientApplicationMVC/Controllers/CompanyReviewController.cs
+++ ClientApplicationMVC/Controllers/CompanyReviewController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ClientApplicationMVC.Controllers
 {
@@ -21,9 +22,30 @@ namespace ClientApplicationMVC.Controllers
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             // TODO: Put the actual URI for the assignment 4 instance here
-            var result = client.PostAsync("http://localhost/api/Reviews", content).Result;
+            var result = client.PostAsync("http://localhost/api/Reviews/", content).Result;
+
+            if(result.IsSuccessStatusCode)
+            {
+
+            }
+
+            else
+            {
+
+            }
 
             return View();
+        }
+
+        // GET: Company Reviews
+        public HttpResponseMessage GetCompanyReview(String companyName) 
+        {
+            HttpClient client = new HttpClient();
+
+            // TODO: Put actual URI of assignment 4
+            var result = client.GetAsync("http://localhost/api/Reviews/" + companyName);
+
+            return result.Result;
         }
     }
 }
