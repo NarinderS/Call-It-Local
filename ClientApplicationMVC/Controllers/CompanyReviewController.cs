16363d3e9e7482d891e012df2f583b92d0093f25
Unknown <harjeej5@gmail.com>
2018-03-28 21:09:21 -0600

    Adding da API

---
+++ ClientApplicationMVC/Controllers/CompanyReviewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientApplicationMVC.Controllers
+{
+    public class CompanyReviewController : Controller
+    {
+        // POST: CompanyReview
+        [HttpPost]
+        public ActionResult PostCompanyReview(String companyName, String review, String stars, String username)
+        {
+            return View();
+        }
+    }
+}
