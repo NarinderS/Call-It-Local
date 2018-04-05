e2c58946136d220561b8505c99b9db6c90800dc8
Sadat Islam <sadat21@hotmail.ca>
2018-04-04 19:17:00 -0600

    Renamed Huba to Hubs

--- ClientApplicationMVC/Hubs/ChatHub.cs
+++ ClientApplicationMVC/Hubs/ChatHub.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
-namespace ClientApplicationMVC.Huba
+namespace ClientApplicationMVC.Hubs
 {
     public class ChatHub : Hub
     {
