ae6bb0890c816d8ca8bfb4cd94b78ffd92b47ec3
Unknown <harjeej5@gmail.com>
2018-04-11 10:12:11 -0600

    Added the methods for weatherservice

--- WeatherService/Methods/WeatherMethods.cs
+++ WeatherService/Methods/WeatherMethods.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace WeatherService.Methods
 {
     class WeatherMethods
     {
         //Harjje add methods
+        public WeatherReturnObject getWeatherStuff(string loc)
+        {
+            string locations = getCompanyLocation(loc);
+
+            WeatherReturnObject returnObj = new WeatherReturnObject();
+        }
+
+        private string getCompanyLocation(string loc)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = client.GetAsync("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=g7mFC2QG1JgcakSVgunj3WoAXXVtLcIV&q=" + loc).Result;
+
+            string rValue = response.Content.ReadAsStringAsync().Result;
+
+            return rValue;
+        }
     }
 }
